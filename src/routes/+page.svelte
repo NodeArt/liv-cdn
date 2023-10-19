fde437fac15ab8d47bb2e6beff5004c8cbc2f698
NosovK <nosov@nodeart.io>
2023-10-19 17:40:40 +0200

    power-bi embed demo
    (cherry picked from commit d7833a27950d1820382d2f461b6590585b224b99)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,4 @@
 <a href="/upload" role="button">Upload test page</a>
 <a href="/camera" role="button">Camera test page</a>
 <a href="/pdf" role="button">Pdf test page</a>
+<a href="/power-bi" role="button">PowerBI test page</a>
