5ee1ef83f6f82c633a70d540cfe96ffff8898a60
NosovK <nosov@nodeart.io>
2023-12-19 13:57:04 +0100

    add /news feed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,6 @@
 <a href="/camera" role="button">Camera test page</a>
 <a href="/pdf" role="button">Pdf test page</a>
 <a href="/power-bi" role="button">PowerBI test page</a>
+<a href="/news" role="button">RSS parser html</a>
+<a href="/api/news" role="button">RSS parser api</a>
 <a href="/links" role="button">onelinks test page</a>
---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" xmlns="http://www.w3.org/1999/html">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>News:</h1>
+{#each data.data as article}
+	<hr />
+	<h2><strong>Title</strong>: {article.title}</h2>
+	<p><strong>Id:</strong> {article.id}</p>
+	<p><strong>Thumbnail:</strong> <img src={article.thumbnail} /></p>
+	<p><strong>Category:</strong> {JSON.stringify(article.category)}</p>
+	<p><strong>Description:</strong></p>
+	<p>{article.description}</p>
+	<p><strong>Author:</strong> {article.author}</p>
+	<p>link to original: <a href={article.link}>{article.link}</a></p>
+	<p><strong>content</strong>:</p>
+	<p>{article.content}</p>
+	<p><strong>Date linux:</strong>{article.published}</p>
+	<p><strong>Date human:</strong> or {new Date(article.published * 1000)}</p>
+{/each}
+
+<h1>Data:</h1>
+<pre>{JSON.stringify(data)}</pre>
+
+<style>
+	img {
+		max-height: 200px;
+	}
+</style>
