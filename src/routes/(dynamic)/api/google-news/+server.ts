14547ebf253d6f166690a5989106f71ae5730474
NosovK <nosov@nodeart.io>
2023-12-19 18:01:33 +0100

    add google/rss

---
+++ src/routes/(dynamic)/api/google-news/+server.ts
@@ -0,0 +1,33 @@
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { parseRSS } from './parser';
+import { html2Markdown } from '@inkdropapp/html2markdown';
+
+export const GET = (async ({ fetch }) => {
+	const feed = await parseRSS(
+		'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FuSjFHZ0pWUVNnQVAB?hl=ru&gl=UA&ceid=UA%3Aru',
+		fetch
+	);
+	const result = feed.items.map((item) => {
+		return {
+			id: item.id,
+			title: html2Markdown(item.title),
+			description: item.description,
+			author: item.author,
+			link: item.link,
+			published: item.published,
+			created: item.created,
+			category: item.category,
+			content: html2Markdown(item.content ?? item.fullText, {})
+			// content_encoded: html2Markdown(item.content_encoded)
+		};
+	});
+	return new Response(JSON.stringify(result), {
+		headers: {
+			'Content-Type': 'application/json',
+			'Cache-Control': `s-maxage=3000`,
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+		}
+	});
+}) satisfies RequestHandler;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,20 @@
 <h1>Test page</h1>
 <hr />
 <main class="container">
-<a href="/upload" role="button">Upload test page</a>
-<a href="/camera" role="button">Camera test page</a>
-<a href="/pdf" role="button">Pdf test page</a>
-<a href="/power-bi" role="button">PowerBI test page</a>
-<a href="/news" role="button">RSS parser html</a>
-<a data-sveltekit-reload href="/api/news" role="button">RSS parser api</a>
-<a href="/links" role="button">onelinks test page</a>
+	<a href="/upload" role="button">Upload test page</a>
+	<a href="/camera" role="button">Camera test page</a>
+	<a href="/pdf" role="button">Pdf test page</a>
+	<a href="/power-bi" role="button">PowerBI test page</a>
+	<a href="/news" role="button">RSS parser html</a>
+	<a data-sveltekit-reload href="/api/news" role="button">RSS parser api</a>
+	<a href="/google-news" role="button">Google RSS</a>
+	<a href="/links" role="button">onelinks test page</a>
 </main>
+
 <style>
-    a {
-        padding: 10px;
-        width: 30%;
-        margin: 10px;
-    }
+	a {
+		padding: 10px;
+		width: 30%;
+		margin: 10px;
+	}
 </style>
