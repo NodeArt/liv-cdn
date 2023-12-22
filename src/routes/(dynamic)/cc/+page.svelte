4890a99c9f13e2d232b7bf46434a836ec07fa653
NosovK <nosov@nodeart.io>
2023-12-22 01:31:11 +0100

    reorder buttons

--- src/routes/(dynamic)/cc/+page.svelte
+++ src/routes/(dynamic)/cc/+page.svelte
@@ -14,7 +14,7 @@
 	<input type="submit" value="recreate processing query" />
 </form>
 
-<table>
+<table role="grid">
 	<caption> CC</caption>
 	<thead>
 		<tr>
@@ -32,15 +32,6 @@
 			{#each data?.records as rec, i (rec.uid)}
 				<tr>
 					<th scope="row">{i + 1}</th>
-					<td class="truncate hidden">{rec.uid}</td>
-					<td>{rec.name}</td>
-					<td
-						><a target="_blanc" href="https://qdpro.com.ua/uk/uktzed_search/{rec.code}"
-							>{rec.code}</a
-						></td
-					>
-					<td>{rec.match}</td>
-					<td><input type="checkbox" bind:checked={rec.answer} /></td>
 					<td>
 						<form
 							on:submit={send}
@@ -59,10 +50,40 @@
 							action="?/add"
 						>
 							<input type="hidden" value={rec.uid} name="uid" />
-							<input type="hidden" value={rec.answer} name="answer" />
-							<input type="submit" value="Ok" />
+							<input type="hidden" value="0" name="answer" />
+							<input type="submit" class="contrast" value="не верно" />
 						</form>
 					</td>
+					<td>
+						<form
+							on:submit={send}
+							use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+								return async ({ result, update }) => {
+									if (result?.data?.uid === data.records[i].uid) {
+										data.records.splice(i, 1);
+										data.records = [...data.records];
+									}
+									// update();
+									// `result` is an `ActionResult` object
+									// `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
+								};
+							}}
+							method="post"
+							action="?/add"
+						>
+							<input type="hidden" value={rec.uid} name="uid" />
+							<input type="hidden" value="1" name="answer" />
+							<input type="submit" value="верно" />
+						</form>
+					</td>
+					<td class="truncate hidden">{rec.uid}</td>
+					<td>{rec.name}</td>
+					<td
+						><a target="_blanc" href="https://qdpro.com.ua/uk/uktzed_search/{rec.code}"
+							>{rec.code}</a
+						></td
+					>
+					<td>{rec.match}</td>
 				</tr>
 			{/each}
 		{:else}
@@ -97,27 +118,27 @@
 	}
 
 	thead th:nth-child(1) {
-		width: 5%;
+		width: 10%;
 	}
 
 	thead th:nth-child(2) {
-		width: 10%;
+		width: 7%;
 	}
 
 	thead th:nth-child(3) {
-		width: 20%;
+		width: 15%;
 	}
 	thead th:nth-child(4) {
-		width: 30%;
+		width: 13%;
 	}
 	thead th:nth-child(5) {
-		width: 30%;
+		width: 25%;
 	}
 	thead th:nth-child(6) {
-		width: 5%;
+		width: 30%;
 	}
 	thead th:nth-child(7) {
-		width: 10%;
+		width: 30%;
 	}
 	.truncate {
 		width: 250px;
