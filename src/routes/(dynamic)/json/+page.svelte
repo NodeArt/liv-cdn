d09847e974036b764a78db325018011bddcfbfe7
NosovK <nosov@nodeart.io>
2024-02-23 20:17:44 +0100

    add nested json parse

--- src/routes/(dynamic)/json/+page.svelte
+++ src/routes/(dynamic)/json/+page.svelte
@@ -2,22 +2,29 @@
 	let value = '';
 	let errorMessage = '';
 	let errorNestedJSON = '';
+	let errorSymbol = '';
 
 	const click = async () => {
 		try {
 			const parsed = JSON.parse(value);
 			console.log(parsed);
 			errorMessage = 'No error';
+			errorSymbol = '';
 			try {
 				const content = JSON.parse(parsed[0].message.content);
 				console.log(content);
-				errorNestedJSON = 'content parse error';
+				errorNestedJSON = 'no content parse error';
 			} catch (error) {
+				console.log(/^[^\d]*(\d+)/.exec(error.message)[1]);
 				errorNestedJSON = error.message;
+				let errPos = Number(/^[^\d]*(\d+)/.exec(error.message)[1]);
+				errorSymbol = parsed[0]?.message?.content?.slice(Math.max(0, errPos - 7), errPos + 3);
 			}
 		} catch (error) {
 			errorMessage = error.message;
-			console.log(error);
+			let errPos = Number(/^[^\d]*(\d+)/.exec(error.message)[1]);
+			console.log(Math.max(1, errPos - 7));
+			errorSymbol = value.slice(Math.max(0, errPos - 7), errPos + 3);
 		}
 	};
 </script>
@@ -25,6 +32,7 @@
 <div>
 	<p>Error: {errorMessage}</p>
 	<p>Error nested json [message.content]: {errorNestedJSON}</p>
+	<p>Error Symbol: {errorSymbol}</p>
 	<textarea cols="40" rows="5" bind:value on:input={click} />
 	<button on:click={click}>CLICK</button>
 </div>
