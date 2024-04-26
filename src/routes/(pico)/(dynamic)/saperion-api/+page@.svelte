e494c14631343ed44f79c31315c6b25ad1028adf
NosovK <nosov@nodeart.io>
2024-04-26 02:32:46 +0200

    format

--- src/routes/(pico)/(dynamic)/saperion-api/+page@.svelte
+++ src/routes/(pico)/(dynamic)/saperion-api/+page@.svelte
@@ -1,32 +1,29 @@
 <svelte:head>
-    <script
-            type="text/javascript"
-            src="https://unpkg.com/rapidoc@9.3.4/dist/rapidoc-min.js"
-    ></script>
+	<script type="text/javascript" src="https://unpkg.com/rapidoc@9.3.4/dist/rapidoc-min.js"></script>
 </svelte:head>
 
 <rapi-doc
-        id="thedoc"
-        spec-url="/saperion.json"
-        theme="light"
-        layout="column"
-        render-style="read"
-        allow-spec-url-load="false"
-        allow-spec-file-load="false"
-        allow-server-selection="false"
-        allow-search="false"
-        bg-color="#f0f3f6"
-        nav-bg-color="#e3e8ec"
-        nav-text-color="#999"
-        nav-hover-bg-color="#fff"
-        header-color="#e3e8ec"
-        primary-color="#553adf"
-        text-color="#4d4d4d"
-        regular-font="'Arial', sans-serif"
-        style="height:100vh; width:100%"
-        mono-font="Inconsolata,Monaco,Consolas,'Courier New',Courier,monospace"
+	id="thedoc"
+	spec-url="/saperion.json"
+	theme="light"
+	layout="column"
+	render-style="read"
+	allow-spec-url-load="false"
+	allow-spec-file-load="false"
+	allow-server-selection="false"
+	allow-search="false"
+	bg-color="#f0f3f6"
+	nav-bg-color="#e3e8ec"
+	nav-text-color="#999"
+	nav-hover-bg-color="#fff"
+	header-color="#e3e8ec"
+	primary-color="#553adf"
+	text-color="#4d4d4d"
+	regular-font="'Arial', sans-serif"
+	style="height:100vh; width:100%"
+	mono-font="Inconsolata,Monaco,Consolas,'Courier New',Courier,monospace"
 >
-    <div slot="header">Saperion API documentation</div>
+	<div slot="header">Saperion API documentation</div>
 </rapi-doc>
 
 <style>
