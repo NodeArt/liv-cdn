e224c3dbb6f97ee68bf79aa59dd4d6bc36d99498
NosovK <nosov@nodeart.io>
2024-06-04 14:35:44 +0200

    use env

--- src/lib/utils/banksy.ts
+++ src/lib/utils/banksy.ts
@@ -1,7 +1,7 @@
 import { Banksy } from '@nest25/banksy-sdk';
 //todo: Uncomment to use .env variable. The name of variable should has VITE_ prefix.
-//const BANKSY_CLIENT_KEY = import.meta.env.VITE_BANKSY_CLIENT_KEY;
-const BANKSY_CLIENT_KEY = 'ck_test_f39f8a74-1267-4ce8-a0cb-e19e2597c105';
+const BANKSY_CLIENT_KEY = import.meta.env.VITE_BANKSY_CLIENT_KEY;
+// const BANKSY_CLIENT_KEY = 'ck_test_f39f8a74-1267-4ce8-a0cb-e19e2597c105';
 console.info('Banksy client key:', BANKSY_CLIENT_KEY);
 const banksy = new Banksy(BANKSY_CLIENT_KEY);
 export default banksy;
