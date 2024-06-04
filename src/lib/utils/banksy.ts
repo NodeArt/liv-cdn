caf253667be2d8b322228ba9d5ea35f44a7ca180
osmirnov <=>
2024-06-04 12:33:02 +0300

    feat(banksy) refactor

--- src/lib/utils/banksy.ts
+++ src/lib/utils/banksy.ts
@@ -1,7 +1,38 @@
-import { Banksy } from "@nest25/banksy-sdk";
-//import { BANKSY_CLIENT_KEY } from "$env/dynamic/private"
+import { Banksy } from '@nest25/banksy-sdk';
+//todo: Uncomment to use .env variable. The name of variable should has VITE_ prefix.
 //const BANKSY_CLIENT_KEY = import.meta.env.VITE_BANKSY_CLIENT_KEY;
-const BANKSY_CLIENT_KEY = "ck_test_f39f8a74-1267-4ce8-a0cb-e19e2597c105";
-console.info("Banksy client key:", BANKSY_CLIENT_KEY);
+const BANKSY_CLIENT_KEY = 'ck_test_f39f8a74-1267-4ce8-a0cb-e19e2597c105';
+console.info('Banksy client key:', BANKSY_CLIENT_KEY);
 const banksy = new Banksy(BANKSY_CLIENT_KEY);
 export default banksy;
+
+export type PaymentProvider = {
+	_id: string;
+	name: string;
+	currencyType: string;
+	title?: string
+};
+
+export async function checkPayment(paymentId: string): Promise<boolean> {
+	if (!paymentId) {
+		console.error('No paymentId found in url!');
+		return false;
+	}
+	const paymentData = await banksy.getPayment(paymentId);
+	console.info('Banksy paymentData', paymentData);
+	return paymentData.status === 'success';
+}
+
+export async function createRedirectLink(provider: PaymentProvider, amount: number, successUrl: string, failUrl: string) {
+	const currency = 'USD';
+	const externalClientId = 'tbd';
+	const response = await banksy.createPayment(
+		provider.currencyType,
+		amount,
+		currency,
+		successUrl,
+		failUrl,
+		externalClientId
+	);
+	return response.paymentLink;
+}
--- src/routes/(pico)/(dynamic)/banksy/[[status]]/+page.svelte
+++ src/routes/(pico)/(dynamic)/banksy/[[status]]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import banksy from '$lib/utils/banksy';
+	import banksy, { checkPayment, createRedirectLink, type PaymentProvider } from '$lib/utils/banksy';
 	import { page } from '$app/stores';
 	import { base } from '$app/paths';
 	import { get } from 'svelte/store';
@@ -8,55 +8,26 @@
 	const url = get(page).url;
 	const params = get(page).params;
 	const paymentId = url.searchParams.get('paymentId');
-
-	async function checkPayment() {
-		if (!paymentId) {
-			console.error('No paymentId found in url!');
-			return false;
-		}
-		const paymentData = await banksy.getPayment(paymentId);
-		console.info('Banksy paymentData', paymentData);
-		return paymentData.status === 'success';
-	}
-
-	async function createRedirectLink() {
-		const provider = providers.find(p => p.name === selectedProvider);
-		if (!provider) {
-			console.error('No Provider fond to create link!', 'selected provider name:', selectedProvider);
-			return;
-		}
-		const currentUrl = url + base;
-		const amount = 19999;
-		const currency = "USD";
-		const externalClientId = 'tbd';
-		const successUrl = currentUrl + '/success';
-		const failUrl = currentUrl + '/fail';
-		const response = await banksy.createPayment(
-				provider.currencyType,
-				amount,
-				currency,
-			successUrl,
-			failUrl,
-			externalClientId
-		);
-		console.log(response);
-		return response.paymentLink;
-	}
-
 	let selectedProvider: string;
-	let redirectLink: string;
-	let providers: { _id: string; name: string; currencyType: string; title?: string; }[];
+	let providers: PaymentProvider[];
 	banksy.getProviders().then((data) => {
-		console.log('providers data', data);
 		providers = data;
 		selectedProvider = data[0].name;
 	});
 
-	if (params.status) {
-		checkPayment();
+	if (params.status && paymentId) {
+		checkPayment(paymentId);
 	}
+
 	async function pay() {
-		const link = await createRedirectLink();
+		const provider = providers.find(p => p.name === selectedProvider);
+		const successUrl = url.origin + base + '/success';
+		const failUrl = url.origin + base + '/fail';
+		if (!provider) {
+			console.error('Provider is required to create a payment link!');
+			return;
+		}
+		const link = await createRedirectLink(provider, 20, successUrl, failUrl);
 		if (!link?.length) {
 			console.error('No link created!');
 			return;
@@ -80,9 +51,9 @@
 {#if !params.status}
 	{#if providers?.length}
 		<select name="providers" id="providers">
-				{#each providers as provider (provider._id)}
-					<option value={provider.name}>{provider.title || provider.name}</option>
-				{/each}
+			{#each providers as provider (provider._id)}
+				<option value={provider.name}>{provider.title || provider.name}</option>
+			{/each}
 		</select>
 		<button on:click={pay}>Pay</button>
 	{:else}
